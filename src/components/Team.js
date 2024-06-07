15e296e0df64908b16d196bebb1fb921dd4fb71d
Gabriel Ollero <ollerogabe@gmail.com>
2024-06-07 08:56:13 +0800

    removed extra component

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,11 +1,27 @@
+import React, { useEffect } from 'react';
+import './styles.css';
 
-function tarakiTeam() {
+function TarakiTeam() {
+  useEffect(() => {
+    // Initialize Swiper when component mounts
+      // eslint-disable-next-line no-unused-vars
+      const swiper = new window.Swiper(".mySwiper", {
+          slidesPerView: "auto",
+      centeredSlides: true,
+      spaceBetween: 20,
+      scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: true,
+      },
+    });
+  }, []);
+
   return (
     <div>
       <div className="font-satoshi overflow-x-hidden">
         <div className="cont">
           <section className="mt-16 text-center">
-            <h1 className="font-semibold text-md">Taraki&apos;s Team</h1>
+            <h1 className="font-semibold text-md">Taraki's Team</h1>
             <p className="font-light text-sm px-10 mt-5">
               At TARAKI, our team is dedicated to driving technological
               innovation and fostering a collaborative environment for growth
@@ -13,15 +29,29 @@ function tarakiTeam() {
               shared commitment to our mission.
             </p>
           </section>
-          <section>
-                      <div className="mx-auto mt-5 bg-gray-500 w-72 h-60">{}</div>
-            <p className="text-sm text-center font-light mt-5 px-16">
-              John Doe
-            </p>
+          <section className="mx-auto mt-5">
+            
+            <div className="swiper mySwiper">
+              <div className="swiper-wrapper">
+                <div className="swiper-slide bg-gray-500 text-center w-72 h-40 mx-1">
+                <div></div>
+                </div>
+                              <div className="swiper-slide bg-gray-500 text-center w-72 h-40">
+                                  
+                </div>
+                <div className="swiper-slide bg-gray-500 text-center w-72 h-40">Slide 3</div>
+                <div className="swiper-slide bg-gray-500 text-center w-72 h-40">Slide 4</div>
+                <div className="swiper-slide bg-gray-500 text-center w-72 h-40">Slide 5</div>
+                <div className="swiper-slide bg-gray-500 text-center w-72 h-40">Slide 6</div>
+                <div className="swiper-slide bg-gray-500 text-center w-72 h-40">Slide 7</div>
+              </div>
+              <div className="swiper-scrollbar"></div>
+            </div>
           </section>
         </div>
       </div>
     </div>
   );
 }
-export default tarakiTeam;
+
+export default TarakiTeam;
